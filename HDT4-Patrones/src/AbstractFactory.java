0119e6b64b5e2b1bd769dad8f6eabc08bc0d275b
Jose <Jose@192.168.1.11>
2016-08-19 23:05:10 -0600

    llega el abstrac factroy

--- HDT4-Patrones/src/AbstractFactory.java
+++ HDT4-Patrones/src/AbstractFactory.java
@@ -8,6 +8,20 @@
 
 public class AbstractFactory<E> {
     
-
+    public Stack<E> getStack(String tipo){
+		switch(tipo){
+		case("ArrayList"):
+			return new ArrayLista<E>();
+		case("Vector"):
+			return new Vector<E>();
+		case("List"):
+			return new Lista<E>();
+		
+		default:
+			System.out.println("Error.Saliendo del programa.");
+			System.exit(0);
+			return null;
+		}
+	}
 }
 
